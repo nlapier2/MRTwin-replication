40383ad83558d8011f40752a0902d49c620c136f
Nathan LaPierre <nathanl2012@gmail.com>
2022-11-17 19:07:39 -0600

    refactoring of sim pipeline

--- simulation_ukbb_geno/rand_eff_ukbb_sim.py
+++ simulation_ukbb_geno/rand_eff_ukbb_sim.py
@@ -6,6 +6,7 @@ import subprocess
 import sys
 import numpy as np
 import pandas as pd
+from pandas_plink import read_plink
 import statsmodels.formula.api as smf
 from generate_offspring import generate_offspring
 
@@ -13,7 +14,7 @@ from generate_offspring import generate_offspring
 # declare global-scope output file names. later set to extend --output base name.
 e_causal_fname, o_causal_fname, all_causal_fname = '', '', ''
 ext_iid_fname, parent_iid_fname = '', ''
-ext_plink_fname, trio_plink_fname = '', ''
+ext_plink_fname, trio_plink_fname, combined_plink_fname = '', '', ''
 pheno_df_fname = ''
 gwas_out_fname = ''
 pa1geno_fname, pa2geno_fname = '', ''
@@ -25,7 +26,7 @@ betahat_eo_fname = ''
 def parseargs():    # handle user arguments
     parser = argparse.ArgumentParser(description='Random effects GWAS simulator for UK Biobank genotypes.')
     parser.add_argument('--bfile', required=True, help='Plink bfile base name. Required.')
-    parser.add_argument('--pcfile', required=True, help='tsv file with UKBB individuals mapped to PCs')
+    parser.add_argument('--evecs', required=True, help='Eigenvectors file w/ one SNP per line. Required.')
     parser.add_argument('--h2ge', type=float, default=0.25, help='Heritability of genetic effects on exposure.')
     parser.add_argument('--h2go', type=float, default=0.25, help='Heritability of genetic effects on outcome.')
     parser.add_argument('--h2ce', type=float, default=0.0, help='Heritability of confounder effect on exposure.')
@@ -41,17 +42,21 @@ def parseargs():    # handle user arguments
     return user_args
 
 
-def read_pcfile(pcfile, famfile):
-    fam_df = pd.read_csv(famfile, sep=' ', header=None)
-    pc_df = pd.read_csv(pcfile, sep='\s+', header=None)  # file mapping iids to PCs
-    num_pcs = pc_df.shape[1]
-    pc_df.columns = ['PC' + str(i) for i in range(1, num_pcs + 1)]
-    pc_df['EID'] = fam_df[list(fam_df.columns)[0]]
-    eid_dict = {eid: True for eid in pc_df['EID']}
-    return pc_df, eid_dict
+# compute projections into PC space
+def make_pc_df(bfile, evecs_file):
+    # read eigenvectors and genotype data
+    evecs = np.loadtxt(evecs_file)
+    bedfile, famfile = bfile + '.bed', bfile + '.fam'
+    (bim, fam, bed) = read_plink(bedfile, verbose=False)
+    # project into PC space and store in dataframe
+    proj = np.array(np.matmul(bed.transpose(), evecs))
+    cols = ['PC' + str(i) for i in range(1, num_pcs + 1)]
+    pc_df = pd.DataFrame(proj, columns=cols)
+    pc_df['EID'] = fam['iid']
+    return pc_df
 
 
-def create_confounder_var(pc_df, num_pcs_confound):  # create random non-linear function of PCs
+def create_confounder_var(pc_df, num_pcs_confound):  # confounder = sum of pcs
     conf = pc_df['PC1'] * 0.0  # initialize to zeros
     for i in range(1, num_pcs_confound + 1):
         pc = pc_df['PC'+str(i)]
@@ -90,14 +95,13 @@ def write_causal(e_causal, o_causal):
         subprocess.Popen(['cat', e_causal_fname, o_causal_fname], stdout=outfile).wait()
 
 
-def split_ids(bfile, eid_dict, trio_pct):  # Randomly partition iids into external and parents for trios
+# Randomly partition iids into external and parents for trios and split plink files
+def split_ids_and_plink(bfile, trio_pct):
     # Collect all iids from fam file
     iid_list = []
     with(open(bfile + '.fam', 'r')) as infile:
         for line in infile:
             iid = line.split()[0]
-            if int(iid) not in eid_dict:  # ignore individuals that we don't have PCs for
-                continue
             iid_list.append(iid)
 
     # now make the random picks
@@ -115,10 +119,10 @@ def split_ids(bfile, eid_dict, trio_pct):  # Randomly partition iids into extern
     with(open(parent_iid_fname, 'w')) as outfile:
         for iid in parent_iids:
             outfile.write(iid + ' ' + iid + '\n')
-    subprocess.Popen(['plink', '--bfile', bfile, '--keep', ext_iid_fname, '--extract', all_causal_fname,
-                      '--recode', '12', '--transpose', '--memory', '4096', '--out', ext_plink_fname]).wait()
-    subprocess.Popen(['plink', '--bfile', bfile, '--keep', parent_iid_fname, '--extract', all_causal_fname,
-                      '--recode', '12', '--memory', '4096', '--out', trio_plink_fname]).wait()
+    subprocess.Popen(['plink', '--bfile', bfile, '--keep', ext_iid_fname,
+                      '--make-bed', '--memory', '4096', '--out', '1'+ext_plink_fname]).wait()
+    subprocess.Popen(['plink', '--bfile', bfile, '--keep', parent_iid_fname,
+                      '--recode', '12', '--memory', '4096', '--out', '1'+trio_plink_fname]).wait()
     return ext_iids, parent_iids
 
 
@@ -166,11 +170,9 @@ def sim_children():  # simulate children for trios, add them to plink PED file, 
     child2_geno = generate_offspring(pa1genos, pa2genos, 1)
 
     # append child genos to ped file
-    max_pa_iid = max(iid_list) + 1  # start counting child iids after max parental iid, to avoid duplicates
-    child1_iid_list = [str(max_pa_iid + i) for i in range(len(child1_geno))]
-    child1_iid_dict = {i: True for i in child1_iid_list}
-    child2_iid_list = [str(max_pa_iid + i + len(child1_geno)) for i in range(len(child2_geno))]
-    child2_iid_dict = {i: True for i in child2_iid_list}
+    # max_pa_iid = max(iid_list) + 1  # start counting child iids after max parental iid, to avoid duplicates
+    child1_iid_list = [str(999999999 + i) for i in range(len(child1_geno))]
+    child2_iid_list = [str(999999999 + i + len(child1_geno)) for i in range(len(child2_geno))]
     with(open(trio_plink_fname + '.ped', 'a')) as pedfile:
         static_fields = ' 0 0 1 -9'
         for i in range(len(child1_geno)):
@@ -180,26 +182,23 @@ def sim_children():  # simulate children for trios, add them to plink PED file, 
             plink12_alleles_2 = get_alleles(child2_geno[i])
             iid_fid_2 = child2_iid_list[i] + ' ' + child2_iid_list[i]
             pedfile.write(iid_fid_2 + static_fields + plink12_alleles_2 + '\n')
-    subprocess.Popen(['plink', '--file', trio_plink_fname, '--recode', '12', '--transpose',
-                      '--memory', '4096', '--out', trio_plink_fname]).wait()
-    return child1_iid_dict, child2_iid_dict, pa1genos, pa2genos, child1_geno, child2_geno
+    return child1_iid_list, child2_iid_list, pa1genos, pa2genos, child1_geno, child2_geno
 
 
-def generate_genotypic_effect(tfile, causal_snps):  # generate genotypic effects on phenotypes
+def generate_genotypic_effect(bfile, causal_snps):  # generate genotypic effects on phenotypes
     genotypic_effect = []
-    with(open(tfile + '.tped', 'r')) as infile:
-        for line in infile:
-            splits = line.strip().split()
-            snp_id = splits[1]
-            if snp_id not in causal_snps:
-                continue
-            snp_effect = causal_snps[snp_id]
-            genos = np.array(get_genotypes(splits[4:]))
-            genos = (genos - np.mean(genos)) / np.std(genos)  # standardize
-            if len(genotypic_effect) == 0:
-                genotypic_effect = snp_effect * genos
-            else:
-                genotypic_effect += snp_effect * genos
+    (bim, fam, bed) = read_plink(bfile + ".bed", verbose=False)
+    for i in range(len(bed)):
+        snp_id = bim[bim['i']==i]['snp'].to_string(index=False)
+        if snp_id not in causal_snps:
+            continue
+        snp_effect = causal_snps[snp_id]
+        genos = np.array(bed[i])
+        genos = (genos - np.mean(genos)) / np.std(genos)  # standardize
+        if len(genotypic_effect) == 0:
+            genotypic_effect = snp_effect * genos
+        else:
+            genotypic_effect += snp_effect * genos
     return genotypic_effect
 
 
@@ -222,21 +221,18 @@ def generate_phenotypes(causal_snps, h2g, h2c, confounder, h2eo=None, ext_exposu
     return ext_pheno, trio_pheno
 
 
-def write_child_outcome(child1_iid_dict, child2_iid_dict, e_trio_phenos, o_trio_phenos):
-    fam_df = pd.read_csv(trio_plink_fname + '.tfam', sep=' ', header=None)
-    trio_iids = fam_df[list(fam_df.columns)[0]]
-    child1_e = np.array([e_trio_phenos[i] for i in range(len(e_trio_phenos)) if str(trio_iids[i]) in child1_iid_dict])
-    child1_o = np.array([o_trio_phenos[i] for i in range(len(o_trio_phenos)) if str(trio_iids[i]) in child1_iid_dict])
+def write_child_pheno(child1_iid_list, child2_iid_list, pheno_df):
+    child1_e = [float(pc_df[pc_df['EID'] == id]['exposure']) for id in child1_iid_list]
+    child1_o = [float(pc_df[pc_df['EID'] == id]['outcome']) for id in child1_iid_list]
+    child2_e = [float(pc_df[pc_df['EID'] == id]['exposure']) for id in child2_iid_list]
+    child2_o = [float(pc_df[pc_df['EID'] == id]['outcome']) for id in child2_iid_list]
     child1_phenos = np.array([child1_e, child1_o])
     np.savetxt(child1pheno_fname, child1_phenos)
-    child2_e = np.array([e_trio_phenos[i] for i in range(len(e_trio_phenos)) if str(trio_iids[i]) in child2_iid_dict])
-    child2_o = np.array([o_trio_phenos[i] for i in range(len(o_trio_phenos)) if str(trio_iids[i]) in child2_iid_dict])
     child2_phenos = np.array([child2_e, child2_o])
     np.savetxt(child2pheno_fname, child2_phenos)
 
 
-def merge_phenos_into_df(pc_df, iid_list, exposures, outcomes):
-    pc_df = pc_df[pc_df['EID'].isin(iid_list)]
+def merge_phenos_into_df(pc_df, exposures, outcomes):
     pc_df = pc_df.sort_values(by=['EID'])
     pc_df['exposure'] = exposures
     pc_df['outcome'] = outcomes
@@ -244,34 +240,34 @@ def merge_phenos_into_df(pc_df, iid_list, exposures, outcomes):
     return pc_df
 
 
-def gwas_pass(tfile, pheno_df, num_pcs_regress):
+def gwas_pass(bfile, pheno_df, ext_iids, num_pcs_regress):
     sig_snps, sig_snp_indices = [], []
-    # pc_str = ' + '.join([i for i in pheno_df if 'PC' in i])
     pc_str = ' + '.join(['PC' + str(i) for i in range(1, 1 + num_pcs_regress)])
+    # read in data and extract external portion
+    pheno_df = pheno_df[pheno_df['EID'].isin(ext_iids)]
+    (bim, fam, bed) = read_plink(bfile + ".bed", verbose=False)
+    bed = bed[:,fam[fam['iid'].isin(ext_iids)]['i']]
+    
+    # go through SNPs and perform linear regression including PCs on each
     counter = -1
-    with(open(tfile + '.tped', 'r')) as infile:
-        for line in infile:
-            counter += 1
-            # if counter > 100:  # for debugging purposes
-            #     break
-            splits = line.strip().split()
-            snp_id = splits[1]
-            genos = get_genotypes(splits[4:])
-            pheno_df['genos'] = genos
-            if pc_str != '':
-                ols_res_e = smf.ols(formula='exposure ~ genos + ' + pc_str, data=pheno_df).fit()
-                ols_res_o = smf.ols(formula='outcome ~ genos + ' + pc_str, data=pheno_df).fit()
-            else:  # no principal components used
-                ols_res_e = smf.ols(formula='exposure ~ genos', data=pheno_df).fit()
-                ols_res_o = smf.ols(formula='outcome ~ genos', data=pheno_df).fit()
-            pval_e = ols_res_e.pvalues['genos']
-            if pval_e < 0.001565402:  # F>10 filter; other options are 0.05 or 5*10**-8
-                sig_snp_indices.append(counter)
-                beta_e = ols_res_e.params['genos']
-                se_e = ols_res_e.bse['genos']
-                beta_o = ols_res_o.params['genos']
-                se_o = ols_res_o.bse['genos']
-                sig_snps.append([snp_id, beta_e, se_e, beta_o, se_o, pval_e])
+    for i in range(len(bed)):
+        counter += 1
+        snp_id = bim[bim['i']==i]['snp'].to_string(index=False)
+        pheno_df['genos'] = bed[i]
+        if pc_str != '':
+            ols_res_e = smf.ols(formula='exposure ~ genos + ' + pc_str, data=pheno_df).fit()
+            ols_res_o = smf.ols(formula='outcome ~ genos + ' + pc_str, data=pheno_df).fit()
+        else:  # no principal components used
+            ols_res_e = smf.ols(formula='exposure ~ genos', data=pheno_df).fit()
+            ols_res_o = smf.ols(formula='outcome ~ genos', data=pheno_df).fit()
+        pval_e = ols_res_e.pvalues['genos']
+        if pval_e < 0.001565402:  # F>10 filter; other options are 0.05 or 5*10**-8
+            sig_snp_indices.append(counter)
+            beta_e = ols_res_e.params['genos']
+            se_e = ols_res_e.bse['genos']
+            beta_o = ols_res_o.params['genos']
+            se_o = ols_res_o.bse['genos']
+            sig_snps.append([snp_id, beta_e, se_e, beta_o, se_o, pval_e])
     return sig_snps, sig_snp_indices
 
 
@@ -303,7 +299,7 @@ def write_gwas(sig_snps, sig_snp_indices, pa1genos, pa2genos, child1_geno, child
     np.savetxt(child2geno_fname, child2_geno)
 
 
-if __name__ == '__main__':
+if __name__ == '__m__':
     args = parseargs()
     if args.bfile.endswith('.'):
         args.bfile = args.bfile[:-1]
@@ -319,6 +315,7 @@ if __name__ == '__main__':
     e_causal_fname, o_causal_fname, all_causal_fname = pre + '.causal.e', pre + '.causal.o', pre + '.causal.all'
     ext_iid_fname, parent_iid_fname = pre + '.ext.iids', pre + '.parent.iids'
     ext_plink_fname, trio_plink_fname = pre + '.ext.plink', pre + '.trio.plink'
+    combined_plink_fname = pre + '.comb.plink'
     pheno_df_fname = pre + '.pheno'
     gwas_out_fname = pre + '.gwas.tsv'
     pa1geno_fname, pa2geno_fname = pre + '.pa1geno', pre + '.pa2geno'
@@ -326,32 +323,38 @@ if __name__ == '__main__':
     child2geno_fname, child2pheno_fname = pre + '.child2geno', pre + '.child2pheno'
     betahat_eo_fname = pre + '.betahatEO'
 
-    # read in principal components, sample causal snps, sample external & trio individuals, and sim trio children
-    main_snplist, main_e_causal, main_o_causal = sample_causal_snps(args.bfile, args.h2ge, args.h2go, args.num_causal)
-    write_causal(main_e_causal, main_o_causal)
-    main_ext_iids, main_parent_iids = split_ids(args.bfile, main_eid_dict, args.trio_pct)
-    main_ext_iids, main_parent_iids = [int(i) for i in main_ext_iids], [int(i) for i in main_parent_iids]
-    main_child1_iid_dict, main_child2_iid_dict, \
-        main_pa1genos, main_pa2genos, main_child1_geno, main_child2_geno = sim_children()
+    # sample causal snps, sample external & trio individuals, and sim trio children
+    m_snplist, m_e_causal, m_o_causal = sample_causal_snps(args.bfile, args.h2ge, args.h2go, args.num_causal)
+    write_causal(m_e_causal, m_o_causal)
+    m_ext_iids, m_parent_iids = split_ids_and_plink(args.bfile, args.trio_pct)
+    m_ext_iids, m_parent_iids = [int(i) for i in m_ext_iids], [int(i) for i in m_parent_iids]
+    m_child1_iid_list, m_child2_iid_list, \
+        m_pa1genos, m_pa2genos, m_child1_geno, m_child2_geno = sim_children()
 
-    # read in PCs and generate confounder
-    main_pc_df, main_eid_dict = read_pcfile(args.pcfile, args.bfile + '.fam')
-    main_pc_df = create_confounder_var(main_pc_df, args.num_pcs_confound)  # add confounder var affected by PCs to pc_df
+    # merge trio & external into single bfile, then compute PC scores and generate confounder
+    # write temporary 0 phenos so we have a tfam file
+    subprocess.Popen(['plink', '--bfile', '1'+ext_plink_fname, '--merge', '1'+trio_plink_fname,
+                          '--make-bed', '--memory', '4096', '--out', '1'+combined_plink_fname]).wait()
+    m_pc_df = make_pc_df('1'+combined_plink_fname, args.evecs)
+    m_pc_df = create_confounder_var(m_pc_df, args.num_pcs_confound)
+    subprocess.Popen(['plink', '--bfile', '1'+combined_plink_fname, '--extract', all_causal_fname,
+                      '--make-bed', '--memory', '4096', '--out', combined_plink_fname]).wait()
 
     # run phenotype simulation and store the results in a DataFrame with the principal components
-    main_ext_confounder = np.array(main_pc_df[main_pc_df['EID'].isin(main_ext_iids)]['conf'])
-    main_e_ext, main_e_trio = generate_phenotypes(main_e_causal, args.h2ge, args.h2ce, main_ext_confounder)
-    main_o_ext, main_o_trio = generate_phenotypes(main_o_causal, args.h2go, args.h2co, main_ext_confounder,
-                                                  h2eo=args.h2eo, ext_exposure=main_e_ext, trio_exposure=main_e_trio)
-    write_child_outcome(main_child1_iid_dict, main_child2_iid_dict, main_e_trio, main_o_trio)
-    main_pheno_df = merge_phenos_into_df(main_pc_df, main_ext_iids, main_e_ext, main_o_ext)
+    m_confounder = np.array(main_pc_df['conf'])
+    m_exposure = generate_phenotypes(m_e_causal, args.h2ge, args.h2ce, m_confounder)
+    m_outcome = generate_phenotypes(m_o_causal, args.h2go, args.h2co, m_confounder,
+                                            h2eo=args.h2eo, exposure=m_exposure)
+    m_pheno_df = merge_phenos_into_df(m_pc_df, m_exposure, m_outcome)
+    m_ext_pheno_df = m_pheno_df[m_pheno_df['EID'].isin(m_ext_iids)]
+    write_child_pheno(m_child1_iid_list, m_child2_iid_list, m_pheno_df)
 
     # run GWAS on the simulated phenotypes, unless the user has requested otherwise
     if not args.no_gwas:
         print('\nRunning GWAS...')
-        main_sig_snps, main_sig_snp_indices = gwas_pass(ext_plink_fname, main_pheno_df, args.num_pcs_regress)
-        write_gwas(main_sig_snps, main_sig_snp_indices,
-                   main_pa1genos, main_pa2genos, main_child1_geno, main_child2_geno)
+        m_sig_snps, m_sig_snp_indices = gwas_pass(ext_plink_fname, m_pheno_df, m_ext_iids, args.num_pcs_regress)
+        write_gwas(m_sig_snps, m_sig_snp_indices,
+                   m_pa1genos, m_pa2genos, m_child1_geno, m_child2_geno)
 
     # clean up files that we no longer need
     rm_cmd = ['rm'] + glob.glob(pre + '*plink*') + \
